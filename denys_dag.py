54fc8c87723ce6f0653586399fb9d5634e07e9c8
dlont <dlont88@gmail.com>
2022-09-09 09:46:31 +0000

    Submittable DAG

--- denys_dag.py
+++ denys_dag.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
+import boto3
+
 from airflow import DAG
-     
-from airflow.providers.amazon.aws.operators.batch import BatchOperator
+from airflow.decorators import task
+from airflow.models.baseoperator import chain
+from airflow.providers.amazon.aws.operators.batch import AwsBatchOperator
+
 
 DAG_TAGS = ['denys_dag']
 
@@ -9,55 +15,23 @@ BATCH_JOB_QUEUE_NAME = 'academy-capstone-summer-2022-job-queue'
 BATCH_JOB_DEFINITIOIN_NAME = 'denys_lontkovskyi-app'
 
 JOB_OVERRIDES = {}
-# JOB_FLOW_OVERRIDES = {
-#     'Name': 'my-demo-cluster',
-#     'ReleaseLabel': 'emr-5.30.1',
-#     'Applications': [
-#         {
-#             'Name': 'Spark'
-#         },
-#     ],    
-#     'Instances': {
-#         'InstanceGroups': [
-#             {
-#                 'Name': "Master nodes",
-#                 'Market': 'ON_DEMAND',
-#                 'InstanceRole': 'MASTER',
-#                 'InstanceType': 'm5.xlarge',
-#                 'InstanceCount': 1,
-#             },
-#             {
-#                 'Name': "Slave nodes",
-#                 'Market': 'ON_DEMAND',
-#                 'InstanceRole': 'CORE',
-#                 'InstanceType': 'm5.xlarge',
-#                 'InstanceCount': 2,
-#             }
-#         ],
-#         'KeepJobFlowAliveWhenNoSteps': False,
-#         'TerminationProtected': False,
-#         'Ec2KeyName': 'mykeypair',
-#     },
-#     'VisibleToAllUsers': True,
-#     'JobFlowRole': 'EMR_EC2_DefaultRole',
-#     'ServiceRole': 'EMR_DefaultRole'
-# }
+
 
 with DAG(
     dag_id='denys_dag_id',
-    schedule_interval='@none',
+    schedule_interval='*/5 * * * *',
     start_date=datetime(2022, 9, 1),
     tags=DAG_TAGS,
     catchup=False,
 ) as dag:
 
 
-    submit_batch_job = BatchOperator(
+    submit_batch_job = AwsBatchOperator(
         task_id='submit_batch_job',
         job_name=BATCH_JOB_NAME,
         job_queue=BATCH_JOB_QUEUE_NAME,
         job_definition=BATCH_JOB_DEFINITIOIN_NAME,
         overrides=JOB_OVERRIDES,
         # Set this flag to False, so we can test the sensor below
-        wait_for_completion=False,
+        # wait_for_completion=False,
     )
